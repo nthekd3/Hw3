f704457efe29cb8918eb7dc85881f90da9a517bb
nthekd3 <44103681+nthekd3@users.noreply.github.com>
2019-10-11 11:40:38 -0500

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+  //create some space for our strings
+  char line[500];
+  char argsarray[20][100];
+
+  //print prompt
+  printf("CS361>");
+  //read line from terminal
+  fgets(line, 500, stdin);
+
+  //break the string up into words
+  char *word = strtok(line, " ");
+  int i = 0;
+  while (word) {
+    printf("word: %s\n", word);
+    //copy a word to the arg array
+    strcpy(argsarray[i], word);
+    //get next word
+    word = strtok(NULL, " ");
+    i = i + 1;
+  }
+
+//   //print out our array
+//   int j=0; 
+//   for (;j<i;j++){
+//     printf("argsarray[%d]: %s\n", j, argsarray[j]);
+//   }  
+}
